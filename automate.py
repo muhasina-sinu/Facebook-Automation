c29e940ee564dd6ceec2fc5b795f43f12d0b1c72
muhasina-sinu <121364702+muhasina-sinu@users.noreply.github.com>
2023-05-30 15:47:09 +0400

    send quotes with the given inputs
    inputs are username,password,number of quotes,time interval between quotes and the recipient facebook id

--- automate.py
+++ automate.py
@@ -7,9 +7,11 @@ from selenium.webdriver.common.by import By
 import time
 import random
 import requests
+#import getpass
+import pwinput
 from bs4 import BeautifulSoup
 
-def inspire():
+def inspire(number,interval):
     site = requests.get('https://quotes.toscrape.com/')
     #print(result.status_code)
     #check the connection to ensure that the website returned content rather than an error.
@@ -17,40 +19,49 @@ def inspire():
     soup = BeautifulSoup(site.text, 'html.parser')
     result = soup.find_all("span",{"class":"text"})
     author = soup.find_all("small",{"class":"author"})
-    for a, b in zip(result, author):
-        print(a.text)
-        print(b.text)
-
-def login():
+    
+    i=0
+    
+    for a, b in zip(result, author):    
+        message = WebDriverWait(browser,20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div[aria-label='Message']")))
+        message.send_keys(a.text,b.text,Keys.RETURN)
+        i=i+1
+        if i == int(number):
+            break
+        time.sleep(int(interval))            
+                
+def login(uname,pwd):
     browser.get('https://www.facebook.com/')
     username = browser.find_element(By.ID, "email")
-    username.send_keys("zarabatulmujeeb@gmail.com")
+    username.send_keys(uname)
     password = browser.find_element(By.ID, "pass")
-    password.send_keys("Zara_hawwa")
+    password.send_keys(pwd)
     submit   = browser.find_element(By.NAME, "login")
     submit.click()
     
-def sender():
+def sender(name):
     newmessage = WebDriverWait(browser,20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div[aria-label='New message']")))
     newmessage.click()
     search= WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "input[aria-label='Send message to']")))
-    search.send_keys("muhasina jalal")
+    search.send_keys(name)
     reciever = WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "a[role = 'presentation']")))
     reciever.send_keys(Keys.RETURN)
 
-    
-
+ 
+uname = input("Enter the username: ")
+pwd = pwinput.pwinput("Password: ")
+number = input("Enter the number of quotes to send : ")
+interval = input("Enter the time gap(in seconds) between quotes: ")
+name = input("Enter the recipient facebook id : ")
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 options.add_argument('--disable-notifications')
 browser = webdriver.Chrome(options=options)
 
-
-login()
-sender()
-
-#message = WebDriverWait(browser,20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div[aria-label='Message']")))
+login(uname,pwd)
+sender(name)
+inspire(number,interval)
 
 
 
