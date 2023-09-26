5fb09bbdf90ded5df2fff4a28ebf220249e93d6f
muhasina-sinu <121364702+muhasina-sinu@users.noreply.github.com>
2023-09-26 20:29:41 +0530

    comments added

--- automate.py
+++ automate.py
@@ -1,35 +1,41 @@
+# Import necessary libraries
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-#from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 import random
 import requests
-#import getpass
-import pwinput
+import pwinput # Library for securely entering passwords
 from bs4 import BeautifulSoup
 
+
+# Function to send inspirational quotes
 def inspire(number,interval):
+    # Send a GET request to the quotes website and parse the HTML content
     site = requests.get('https://quotes.toscrape.com/')
-    #print(result.status_code)
-    #check the connection to ensure that the website returned content rather than an error.
-    #The code returned us a status of 200
     soup = BeautifulSoup(site.text, 'html.parser')
+    
+    # Extract quotes and authors from the HTML content
     result = soup.find_all("span",{"class":"text"})
     author = soup.find_all("small",{"class":"author"})
     
     i=0
-    
+
+    # Loop through the quotes and send them
     for a, b in zip(result, author):    
+        # Wait for the message input field to be visible
         message = WebDriverWait(browser,20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div[aria-label='Message']")))
+        # Send the quote and author's name
         message.send_keys(a.text,b.text,Keys.RETURN)
         i=i+1
         if i == int(number):
             break
+        # Wait for the specified interval
         time.sleep(int(interval))            
-                
+
+# Function to log in to Facebook
 def login(uname,pwd):
     browser.get('https://www.facebook.com/')
     username = browser.find_element(By.ID, "email")
@@ -38,7 +44,8 @@ def login(uname,pwd):
     password.send_keys(pwd)
     submit   = browser.find_element(By.NAME, "login")
     submit.click()
-    
+
+# Function to send messages on Facebook
 def sender(name):
     newmessage = WebDriverWait(browser,20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div[aria-label='New message']")))
     newmessage.click()
@@ -47,18 +54,20 @@ def sender(name):
     reciever = WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "a[role = 'presentation']")))
     reciever.send_keys(Keys.RETURN)
 
- 
+# Get user inputs for Facebook login, recipient, and quote sending
 uname = input("Enter the username: ")
-pwd = pwinput.pwinput("Password: ")
+pwd = pwinput.pwinput("Password: ")  # Securely enter the password
 number = input("Enter the number of quotes to send : ")
 interval = input("Enter the time gap(in seconds) between quotes: ")
 name = input("Enter the recipient facebook id : ")
 
+# Configure Chrome WebDriver options
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 options.add_argument('--disable-notifications')
 browser = webdriver.Chrome(options=options)
 
+# Log in to Facebook, send messages, and send quotes
 login(uname,pwd)
 sender(name)
 inspire(number,interval)
